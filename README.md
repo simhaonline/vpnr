6faefdc0c473aa2b69f43a88e9b1c350d383d1d3
Alessandro Paganelli <apaganelli@empirix.com>
2020-03-11 11:28:21 +0100

    First basic version.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# vpnr
+g vpnr
+
 VPNr is a simple helper to setup a SSH reverse tunnel using a public SSH proxy.
+
+Used to setup a SSH reverse tunnel to a known public server.
+The topology is as follows:
+
+ A (behind firewall or not directly reachable)
+ |
+ |
+ B (public server with sshd)
+ |
+ |
+ C (client)
+
+With this script we can setup a reverse tunnel from A to B so that C can connect
+to A through B.
+
---
+++ vpnr.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Author: Alessandro Paganelli (alessandro.paganelli@gmail.com)
+# 2020/3/11
+#
+#------------------------------------------------------------------------------
+# Used to setup a SSH reverse tunnel to a known public server.
+# The topology is as follows:
+#
+# A (behind firewall or not directly reachable)
+# |
+# |
+# B (public server with sshd)
+# |
+# |
+# C (client)
+#------------------------------------------------------------------------------
+
+# This is the port used by SSHd on A 
+HIDDEN_NODE_PORT=22
+# This is the port used by SSHd on B that we (C) use to connect to A through B (i.e., ssh -p $PROXYPORT user@B
+REVERSE_TUNNEL_PORT=6644
+# This is the port used by SSHd on B to listen for incoming SSH connections. Normally it is 22.
+PROXY_SSHD_PORT=22
+
+# User on B
+USER_B=user
+
+# B fqdn or IP
+PROXY=publicip.example.org
+
+#------------------------------------------------------------------------------
+# Actual script
+
+echo "------------------------------"
+echo "        VPNr started"
+echo
+echo "Using proxy:                   $PROXY"
+echo "Proxy ssh port:                $PROXY_SSHD_PORT"
+echo "Reverse tunnel ssh port:       $REVERSE_TUNNEL_PORT"
+echo "Remote (hidden) node ssh port: $HIDDEN_NODE_PORT"
+
+
+ssh -Ng -R *:$REVERSE_TUNNEL_PORT:localhost:$HIDDEN_NODE_PORT -p $PROXY_SSHD_PORT $USER_B@$PROXY
+
+
